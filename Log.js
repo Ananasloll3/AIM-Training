e85d3e6a32c92b70407017106561f6f480b3bd8b
Ananasloll3 <maxime.avenas68@gmail.com>
2025-02-11 11:48:56 +0100

    Utilisation de chalk@ pour pouvoir utiliser require facilement

--- Log.js
+++ Log.js
@@ -1,16 +1,20 @@
+const chalk = require('chalk');
+
+
 class LogAim {
     constructor() {
-        
     }
 
     info(text) {
+
         const now = new Date();
         const hours = now.getHours();
         const minutes = now.getMinutes();
         const seconds = now.getSeconds();
 
-        console.log(`INFO ${hours}:${minutes}:${seconds} : ${text}`);
+        console.log(chalk.blue(`[INFO] ${hours}:${minutes}:${seconds} : ${text}`));
     }
 }
+
 
 module.exports = LogAim;
