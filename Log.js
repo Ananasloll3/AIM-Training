28f4ddde2b9afbaac3b6ca4021e2c50831116481
Ananasloll3 <maxime.avenas68@gmail.com>
2025-02-11 12:02:03 +0100

    Ajout des fonction error, succes, warning

--- Log.js
+++ Log.js
@@ -12,7 +12,9 @@ function getDate() {
 const Logger = {
 
     info: (text) => console.log(chalk.blue(`[INFO] ${getDate()} : ${text}`)),
-    error: (text) => console.log(chalk.red(`[ERROR] ${getDate()} : ${text}`))
+    error: (text) => console.log(chalk.red(`[ERROR] ${getDate()} : ${text}`)),
+    succes: (text) => console.log(chalk.green(`[SUCCES] ${getDate()} : ${text}`)),
+    warning: (text) => console.log(chalk.yellow(`[WARNING] ${getDate()} : ${text}`))
     
 };
 
