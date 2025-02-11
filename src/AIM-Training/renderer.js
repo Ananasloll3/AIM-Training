a5d07e03476b02e6f23a156455c8043d9aa8e7ea
Ananasloll3 <maxime.avenas68@gmail.com>
2025-02-11 11:49:57 +0100

    La parametre envoyer lors du chrgement des page et maitenant le nom de la page

--- src/AIM-Training/renderer.js
+++ src/AIM-Training/renderer.js
@@ -60,13 +60,13 @@ btn_deplacer.addEventListener("click", () => {
 buttonLoadMenu.addEventListener('click', () => {
     console.log("Changement de la page menu");
     
-    AIM_TRAINING_API.loadPageMenu("1"); // Charge la page MENU depuis electron, le parametre permet de savoir de qui viens la demande
+    AIM_TRAINING_API.loadPageMenu("AIM"); // Charge la page MENU depuis electron, le parametre permet de savoir de qui viens la demande
 });
 
 buttonLoadAim.addEventListener("click", () => {
     console.log("Chargement de la page AIM");
     
-    AIM_TRAINING_API.loadPageAim("1"); // Charge la page AIM depuis electron, le parametre permet de savoir de qui viens la demande
+    AIM_TRAINING_API.loadPageAim("AIM"); // Charge la page AIM depuis electron, le parametre permet de savoir de qui viens la demande
 })
 
 
