43cbc955703d3f1cb0b288113fd74dc90c3e01fc
Ananasloll3 <maxime.avenas68@gmail.com>
2025-02-11 11:19:45 +0100

    Ajout du fichier gerant les log de main.js, ajout de la fonction info

---
+++ Log.js
@@ -0,0 +1,15 @@
+class LogAim {
+    constructor() {
+        
+    }
+
+    info(text) {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+        console.log(`INFO ${hours}:${minutes}:${seconds}`);
+        
+    }
+}
